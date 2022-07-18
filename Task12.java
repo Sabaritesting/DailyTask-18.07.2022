9ee67fa604f4676387fdc96af945da96d0a51e48
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-18 12:57:26 +0530

    Add files via upload

---
+++ Task12.java
@@ -0,0 +1,25 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the first number");
+int a=sc.nextInt();
+System.out.println("Enter the second number");
+int b=sc.nextInt();
+System.out.println("Enter the third number");
+int c=sc.nextInt();
+System.out.println("Enter the fourth number");
+int d=sc.nextInt();
+System.out.println("Enter the fifth number");
+int e=sc.nextInt();
+System.out.println("The average of five numbers are"+(a+b+c+d+e)/5);
+
+
+	}
+
+}
