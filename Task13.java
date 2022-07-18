18e0295aecdd519ccb2dd981cce2baa30a55a772
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-18 12:58:01 +0530

    Add files via upload

---
+++ Task13.java
@@ -0,0 +1,17 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the length of the rectangle");
+int a=sc.nextInt();
+System.out.println("Enter the breadth of the rectangle");
+int b=sc.nextInt();
+System.out.println("The area of rectangle is "+a*b);
+	}
+
+}
