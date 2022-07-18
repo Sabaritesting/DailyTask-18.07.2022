b21f84def708666c52f419ea56935d76152855fb
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-18 12:59:00 +0530

    Add files via upload

---
+++ Task14.java
@@ -0,0 +1,23 @@
+package Tasks;
+
+public class Task14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("* * * * * * * ========");
+System.out.println("* * * * * * * ========");
+System.out.println("* * * * * * * ========");
+System.out.println("* * * * * * * ========");
+System.out.println("* * * * * * * ========");
+System.out.println("* * * * * * * ========");
+System.out.println("* * * * * * * ========");
+System.out.println("* * * * * * * ========");
+System.out.println("======================");
+System.out.println("======================");
+System.out.println("======================");
+System.out.println("======================");
+System.out.println("======================");
+System.out.println("======================");
+	}
+
+}
