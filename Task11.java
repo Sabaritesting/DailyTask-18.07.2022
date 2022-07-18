defef19f230baafa1e4f7e78fa5d5ce27f563574
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-18 12:57:02 +0530

    Add files via upload

---
+++ Task11.java
@@ -0,0 +1,14 @@
+package Tasks;
+
+public class Task11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double radius=25.25;
+double perimeter=2*Math.PI*radius;
+double area=Math.PI*radius*radius;
+System.out.println("The perimeter of the circle is" +perimeter);
+System.out.println("The area of the circle is"+area);
+	}
+
+}
