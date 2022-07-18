3edeb6c93d39af7d93e875c707299b27eb137950
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-18 12:59:43 +0530

    Add files via upload

---
+++ Task15.java
@@ -0,0 +1,16 @@
+package Tasks;
+
+public class Task15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=15,b=4,car;
+System.out.println("Before swapping : a, b = "+a+", "+ + b);
+car=a;
+a=b;
+b=car;
+System.out.println("After swapping : a, b = "+a+", "+ + b);
+
+	}
+
+}
